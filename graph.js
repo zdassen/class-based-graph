286e5f7d9825878de091e84c1ccd18f13caf7bbf
zdassen <e3eas9lt1wuyifw@yahoo.co.jp>
2018-06-26 20:57:09 +0900

    add data getter

--- graph.js
+++ graph.js
@@ -41,6 +41,12 @@
     // x 方向の罫線のスタイルを設定する
     this.setYGridStyle();
 
+    /* x 軸のパスのスタイルを設定する */
+    this.setXAxisPathStyle();
+
+    /* y 軸のパスのスタイルを設定する */
+    this.setYAxisPathStyle();
+
   }
 
   /* マージンをチェックする ( 不正な値が指定されていないか ) */
@@ -89,7 +95,8 @@
     // 数値の場合
     let x;
     if (Graph.isNumeric(peek)) {
-      // ※まだ
+      // 数値の場合
+      x = this.setXLinearScale();
     } else if (peek instanceof Date) {
       // 日付の場合
       x = this.setXTimeScale();
@@ -98,13 +105,31 @@
     return x;
   }
 
-  /* x 方向のスケールを設定する */
+  /* x 方向のスケールを設定する ( 数値のスケール ) */
+  setXLinearScale() {
+
+    // 開始 & 終了
+    let start = this.getX(this.data[0]);
+    let finish = this.getX(this.data[this.data.length - 1]);
+
+    // x 方向のスケールを作成する
+    let x = d3.scaleLinear()
+      .domain([start, finish])
+      .range([
+        this.margin.left,
+        this.width - this.margin.right,
+      ]);
+
+    return x;
+  }
+
+  /* x 方向のスケールを設定する ( 日時のスケール ) */
   setXTimeScale() {
 
     // データの先頭の日時 ~ 末尾の日時
     // つまり、日付 & 時刻でソートされていることが前提
-    let start = this.data[0]["at"];
-    let finish = this.data[this.data.length - 1]["at"];
+    let start = this.getX(this.data[0]);
+    let finish = this.getX(this.data[this.data.length - 1]);
 
     // x 方向のスケールを作成する
     let x = d3.scaleTime()
@@ -222,6 +247,23 @@
     this.setGridStyle("y-axis", this.setYAxisColor);
   }
 
+  /* x 軸または y 軸のパスのスタイルを設定する */
+  setAxisPathStyle(axisClass) {
+    this.svg.selectAll(`g.${axisClass} path`)
+      .attr("stroke", "lightgray")
+      .attr("stroke-width", "1px");
+  }
+
+  /* x 軸のパスのスタイルを設定する */
+  setXAxisPathStyle() {
+    this.setAxisPathStyle("x-axis")
+  }
+
+  /* y 軸のパスのスタイルを設定する */
+  setYAxisPathStyle() {
+    this.setAxisPathStyle("y-axis")
+  }
+
 }
 
 
@@ -231,17 +273,41 @@ class LineGraph extends Graph {
   constructor(svgId, data,
     margin={top: 20, right: 20, bottom: 20, left: 32}) {
     super(svgId, data, margin);
+
+    // ※とりあえず動くところを
+    let valueLineFunc = d3.line()
+      .x(d => this.x(d.at))
+      .y(d => this.y(d.value));
+    let valueLine = this.svg.append("path")
+      .data([this.data])
+      .attr("class", "path-aaa")
+      .attr("d", valueLineFunc)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", "1.5px");
+
   }
 
 }
 
 /* main */
 let svgId = "graphArea";
-let data = [
-  {value: 10, at: new Date("2018-06-26 04:45:45")},
-  {value: 13, at: new Date("2018-06-26 07:30:24")},
-  {value: 11, at: new Date("2018-06-26 12:30:37")},
-  {value: 25, at: new Date("2018-06-26 15:25:13")},
-];
+let dataType = "date";
+let data;
+if (dataType === "date") {
+  data = [
+    {value: 10, at: new Date("2018-06-26 04:45:45")},
+    {value: 13, at: new Date("2018-06-26 07:30:24")},
+    {value: 11, at: new Date("2018-06-26 12:30:37")},
+    {value: 25, at: new Date("2018-06-26 15:25:13")},
+  ];
+} else if (dataType === "numeric") {
+  data = [
+    {value: 10, at: 1},
+    {value: 13, at: 5},
+    {value: 11, at: 7},
+    {value: 25, at: 13},
+  ];
+}
 let lg = new LineGraph(svgId, data);
 console.dir(lg);
